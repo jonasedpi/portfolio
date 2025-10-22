ca8afc051e89aecd588af1beb6832ae5e929004f
Jonas Pinheiro <jonasespinheiro@gmail.com>
2025-10-22 18:57:14 -0300

    add i18next in about.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,21 @@
+import { useTranslation } from "react-i18next";
+
 const About = () => {
+
+    const { t, i18n } = useTranslation();
+    
+    const changeLanguage = (lang: string) => {
+        i18n.changeLanguage(lang);
+  };
+
     return(
         <>
+            <button onClick={() => changeLanguage("pt")}>🇧🇷 PT</button>
+            <button onClick={() => changeLanguage("en")}>🇺🇸 EN</button>
             <h2>About</h2>
-            <p>Oi, me chamo Jonas Eduardo Schumann Pinheiro. Estou estudando para ser programador Full-Stack.</p> 
-            <p>Eu estou cursando Sistema de informação na Unisinos.</p>
-            <p>Como serviço extra. Eu faço desenvolvimento de jogos com Unity, Godot e gamemaker e Pixel Art.</p>
+            <p>{t("ab_greeting")}</p> 
+            <p>{t("ab_college")}</p>
+            <p>{t("ab_technologies")}</p>
         </>
     )
 }
