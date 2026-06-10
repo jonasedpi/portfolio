dd92f8242b6b85d27246924872cc5b783ef783bb
Jonas Pinheiro <jonasespinheiro@gmail.com>
2026-06-10 18:46:01 -0300

    Chance service

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,21 +1,21 @@
 import { useTranslation } from "react-i18next";
 
 const Service = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return(
-        <>
-            <h2>{t("ser_services")}</h2>
-            <h3>Landing Page</h3>
-                <p>{t("inProgress")}</p>
-            <h3>Websites</h3>
-                <p>{t("inProgress")}</p>    
-            <h3>Pixel Art</h3>
-                <p>{t("inProgress")}</p>   
-            <h3>Game Maker</h3>
-                <p>{t("inProgress")}</p>
-        </>
-    )
-}
+  return (
+    <>
+      <h2>{t("ser_services")}</h2>
+      <h3>Landing Page</h3>
+      <p>{t("inProgress")}</p>
+      <h3>Websites</h3>
+      <p>{t("inProgress")}</p>
+      <h3>Pixel Art</h3>
+      <p>{t("inProgress")}</p>
+      <h3>{t("ser_gameDev")}</h3>
+      <p>{t("inProgress")}</p>
+    </>
+  );
+};
 
-export default Service
+export default Service;
