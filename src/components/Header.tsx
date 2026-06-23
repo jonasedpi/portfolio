af046dbfc46d356510e3d48feb361e78e099aafa
Jonas Pinheiro <jonasespinheiro@gmail.com>
2026-06-23 16:37:31 -0300

    Update header and service

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,12 +8,12 @@ const Header = () => {
 
 
   return (
-    <div className="flex flex-row justify-items-center-safe">
-      <div className="flex flex-row justify-items-start-safe">
+    <div className="flex flex-row justify-between items-center p-4">
+      <div className="flex flex-row">
       <button onClick={() => changeLanguage("pt")}>🇧🇷 PT</button>
       <button onClick={() => changeLanguage("en")}>🇺🇸 EN</button>
       </div>
-      <ul className="flex flex-row justify-items-end-safe ">
+      <ul className="flex flex-row ">
         <li className="px-4 py-2"> {t("home")} </li>
         <li className="px-4 py-2">{t("ab_me")}</li>
         <li className="px-4 py-2">{t("pr_projects")}</li>
