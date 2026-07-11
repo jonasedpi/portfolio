fe6ba78dac3d26a2ac58f1fa990e27878ddb51af
Jonas pinheiro <jonasespinheiro@gmail.com>
2026-07-11 02:30:46 -0300

    Update service compoment

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -4,7 +4,7 @@ const Service = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 aspect-<3:2>">
       <h2>{t("ser_services")}</h2>
      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       <h3>Landing Page</h3>
