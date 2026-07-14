ac5002c512c44158bf2d254bfeba663f134289b9
Jonas Pinheiro <jonasespinheiro@gmail.com>
2026-07-14 12:15:01 -0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 
 import { useTranslation } from "react-i18next";
+import perfil from "../assets/images/perfil.png";
 
 const About = () => {
 
@@ -8,10 +9,18 @@ const About = () => {
     return(
         <div>
             <p>{t("ab_me")}</p>
-            <img src="../assets/images/perfil.png" alt="Profile Picture" />
-            <p>{t("ab_greeting")}</p> 
-            <p>{t("ab_college")}</p>
-            <p>{t("ab_technologies")}</p>
+            <div className="flex flex-col items-center columns-2xl">   
+                <img
+                    src={perfil}
+                    alt="Profile Picture"
+                    width={200}
+                    height={200}
+                    className="rounded-full mx-auto my-4 justify-end-safe"
+                />
+                <p>{t("ab_greeting")}</p> 
+                <p>{t("ab_college")}</p>
+                <p>{t("ab_technologies")}</p>
+            </div>     
         </div>
     )
 }
