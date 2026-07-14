abb8cda161e18922a516058978736aaab0a477ad
Jonas Pinheiro <jonasespinheiro@gmail.com>
2026-07-14 11:03:44 -0300

    update de photo perfil

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
     return(
         <div>
             <p>{t("ab_me")}</p>
-            <img src="../assets/images/perfil.jpg" alt="Profile Picture"  />
+            <img src="../assets/images/perfil.png" alt="Profile Picture" />
             <p>{t("ab_greeting")}</p> 
             <p>{t("ab_college")}</p>
             <p>{t("ab_technologies")}</p>
